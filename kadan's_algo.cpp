af52f95d0b134cd99be2d0cf95feab509b637341
Anas Ibn Belal <anasibnebelal400@gmail.com>
2025-06-30 00:12:01 +0600

    refactor maximum subarray sum implementation and update function name; add .gitignore for build files

--- kadan's_algo.cpp
+++ kadan's_algo.cpp
@@ -1,23 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-int Mx_sub_Sum(vector<int>&v, int n){
-    int res=v[0];
-    int maxtillnow=v[0];
+int max_Sum(vector<int>&v,int n){
+    int mxsum=v[0],curr_sum=v[0];
     for (int i = 1; i < n; i++)
     {
-        maxtillnow=max(maxtillnow+v[i],v[i]);
-        res=max(maxtillnow,res);
+        curr_sum=max(v[i],curr_sum+v[i]);
+        mxsum=max(mxsum,curr_sum);
+
     }
-    return res;
+    return mxsum;
     
 }
 
-int main()
+void solve()
 {
     int n;cin>>n;
     vector<int>v(n);
     for(int i=0;i<n;i++) cin>>v[i];
-    int ans=Mx_sub_Sum(v,n);
+    int ans=max_Sum(v,n);
     cout<<ans<<endl;
-    return 0;
+}
+int main(){
+    int t;cin>>t;
+    while(t--){
+        solve();
+    }
 }
